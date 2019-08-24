adb2857ab6d18ac89cfbc6cb3ccc72edd8291d4a
Edi Begovic <edibegovic@gmail.com>
2019-08-24 23:58:53 +0200

    Strava Finished

--- scripts/stravastuff.js
+++ scripts/stravastuff.js
@@ -1,38 +1,81 @@
-// $(document).ready(function () {
-//     baseUrl = "https://cors-anywhere.herokuapp.com/https://strava.com/athletes/";
-//     restUrl = "/profile_sidebar_comparison";
+$(document).ready(function () {
+    checkDistance();
+});
 
-//     $.ajax({
-//         url: baseUrl + "15494864" + restUrl,
-//         type: "GET",
-//         dataType: "",
-//         headers: {
-//             'x-requested-with': 'XMLHttpRequest',
-//         },
-//         success: function(response) {
-//             var regexp = new RegExp("<tbody id='running-ytd'>\n<tr>\n<td>Distance<\/td>\n<td>(.*?)<\/td>", "g");
-//             var myVar = regexp.exec(response)[1];
+setInterval(function(){
+    checkDistance();
+}, 900000)
 
-//             console.log(parseInt(myVar.slice(0, -3)));
-//         }
-//     });
+var e_dist = 0;
+var j_dist = 0;
 
-//     $.ajax({
-//         url: baseUrl + "17629999" + restUrl,
-//         type: "GET",
-//         dataType: "",
-//         headers: {
-//             'x-requested-with': 'XMLHttpRequest',
-//         },
-//         success: function(response) {
-//             var regexp = new RegExp("<tbody id='running-ytd'>\n<tr>\n<td>Distance<\/td>\n<td>(.*?)<\/td>", "g");
-//             var myVar = regexp.exec(response)[1];
+function checkDistance() {
+    baseUrl = "https://cors-anywhere.herokuapp.com/https://strava.com/athletes/";
+    restUrl = "/profile_sidebar_comparison";
 
-//             console.log(parseInt(myVar.slice(0, -3)));
-//         }
-//     });
-// });
+    $.ajax({
+        url: baseUrl + "15494864" + restUrl,
+        type: "GET",
+        dataType: "",
+        headers: {
+            'x-requested-with': 'XMLHttpRequest',
+        },
+        success: function(response) {
+            var regexp = new RegExp("<tbody id='running-ytd'>\n<tr>\n<td>Distance<\/td>\n<td>(.*?)<\/td>", "g");
+            var myVar = regexp.exec(response)[1];
 
+            e_dist = (parseInt(myVar.slice(0, -3)));
+            renderDistance();
+        }
+    });
+
+    $.ajax({
+        url: baseUrl + "17629999" + restUrl,
+        type: "GET",
+        dataType: "",
+        headers: {
+            'x-requested-with': 'XMLHttpRequest',
+        },
+        success: function(response) {
+            var regexp = new RegExp("<tbody id='running-ytd'>\n<tr>\n<td>Distance<\/td>\n<td>(.*?)<\/td>", "g");
+            var myVar = regexp.exec(response)[1];
+
+            j_dist = (parseInt(myVar.slice(0, -3)));
+            renderDistance();
+        }
+    });
+}
+
+function renderDistance() {
+    jQuery("#strava").empty();
+
+    var j_col = "#FFFFFF";
+    var e_col = "#FFFFFF";
+
+    let j_len = j_dist/1.8;
+    let e_len = e_dist/1.8;
+
+    if (j_dist > e_dist) {
+        j_col = "#F16B51";
+    } else {
+        e_col = "#F16B51";
+    }
+
+    let html = `
+    <h3 style="margin-left: 46px;">EDI</h3>
+    <div class="distance" style="background-color: ${e_col}; width: ${e_len}px;">
+    <p style="color: ${e_col};">${e_dist}</p>
+    </div>
+    
+    <h3>JONAS</h3>
+    <div class="distance" style="background-color: ${j_col}; width: ${j_len}px;">
+    <p style="color: ${j_col};">${j_dist}</p>
+    </div>
+    `;
+
+    jQuery("#strava").
+    append(html); 
+}
 
 
 
