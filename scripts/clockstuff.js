7a42502610eedc237c3fe2b72bdf1bab80a14880
Edi Begovic <edibegovic@gmail.com>
2019-08-26 22:01:20 +0200

    clock changed

--- scripts/clockstuff.js
+++ scripts/clockstuff.js
@@ -3,7 +3,7 @@ function startTime() {
     var h = today.getHours();
     var m = today.getMinutes();
     m = checkTime(m);
-    document.getElementById('clock').innerHTML = "klokken -->  " + h + ":" + m;
+    document.getElementById('clock').innerHTML = h + ":" + m;
     var t = setTimeout(startTime, 500);
 }
 
