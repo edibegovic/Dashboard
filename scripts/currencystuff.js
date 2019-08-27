223e7cad9bf3a97ea56d1812fad021bc63bc1004
Edi Begovic <edibegovic@gmail.com>
2019-08-27 18:09:27 +0200

    Currency fixed

--- scripts/currencystuff.js
+++ scripts/currencystuff.js
@@ -5,20 +5,17 @@ setInterval(function(){
 function updateCurrency() {
     $.ajax({
         type: "GET",
-        url: "https://cors-anywhere.herokuapp.com/api.exchangeratesapi.io/latest?symbols=DKK&base=SEK",
+        url: "http://data.fixer.io/api/latest?access_key=63a344a2f625759767d8695d505ca4d5&format=1",
         contentType: "application/json",
         dataType: "json",
         headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Origin,X-Requested-With, Content-Type,Accept, Authorization, X-Custom-Header',
-            'Access-Control-Allow-Credentials': 'true'
+            'Content-Type': 'text/plain'
         },
         crossDomain: true,
         success: function (response) {
             if (response == null) {}
             try {
-                document.getElementById('exchangeRate').innerHTML = String(response.rates.DKK).substr(0, 5) + " DKK";
+                document.getElementById('exchangeRate').innerHTML = String(response.rates.DKK*100/response.rates.SEK).substring(0, 6) + " DKK";
             }
             catch (ex) { }
         }
